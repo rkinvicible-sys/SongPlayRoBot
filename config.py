6bf46c94b8570a4843cc5b141707e540d98101f5
rkinvicible-sys <rk.invicible@gmail.com>
2025-09-22 14:37:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("20666532"))
+API_HASH = os.getenv("da5811243de26f755bb80615e34689db")
+BOT_TOKEN = os.getenv("8283865282:AAGS4C4bdTAu7mJo-wOLuBTbQCMoXDU9guo")
 DATABASE_URL = os.getenv("DATABASE_URL")
 OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
